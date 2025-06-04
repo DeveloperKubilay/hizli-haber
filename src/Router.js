39b4eb948741ae0909b79129230cbddfde661bb9
DeveloperKubilay <kubiss52@yahoo.com>
2025-06-05 02:29:04 +0300

    feat: update theme colors in index.html and extend Tailwind configuration for consistent styling

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import './services/index.css'; // CSS dosyasını import ediyoruz
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-background text-textPrimary font-sans">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
