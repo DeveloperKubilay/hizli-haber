ba6b634c6a71c72d1b5eb250ad28b63137e0ec3b
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-06-05 02:29:04 +0300

    feat: update theme colors in index.html and extend Tailwind configuration for consistent styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#16181c', // White
+        primary: '#26292f', //divler
+        primaryBG: '#33363d', //arama kısmı alternatif div
+        secondary: '#1bd96a', //yeşil
+        secondaryHover: '#22ff84', //yeşil hover
+        secondaryBG: '#23553e', //yeşil bg
+        selectBox: '#1a9f52', //seçim kutusu
+
+
+        textHeading: '#ecf9fb', //başlık
+        textPrimary: '#b0bac5', //yazı
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      },
+
+
+    },
   },
   plugins: [],
 }
