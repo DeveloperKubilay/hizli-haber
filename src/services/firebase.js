c4790836e139e5f8bee8a69c5cfe2bba2b408786
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-06-18 20:28:48 +0300

    feat: update news site content structure and integrate AI model for enhanced functionality

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getAI, getGenerativeModel, GoogleAIBackend } from "firebase/ai";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDQXSR04kbpK7-F9VcmShlIELRgdvNCpQI",
@@ -17,5 +18,19 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const ai = getAI(app, { backend: new GoogleAIBackend() });
+const model = getGenerativeModel(ai, { model: "gemini-2.5-flash" });
 
-export { db, auth, analytics };
+async function sendMessageToModel(message = "Hello") {
+  try {
+    const result = await model.generateContent(message);
+    const response = await result.response;
+    const text = response.text();
+    return text;
+  } catch (error) {
+    console.error("Error communicating with Gemini model:", error);
+    return "Error generating response";
+  }
+}
+
+export { db, auth, analytics, model, sendMessageToModel as ai };
