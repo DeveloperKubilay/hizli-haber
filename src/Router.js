c27a08c3d8f4f3134f22b1452b434036e5ef18b8
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-06-23 03:31:57 +0300

    feat: add ProductTicker component with animated product display; implement hover effects and infinite scrolling

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Test from './pages/test';
 import './services/index.css'; // CSS dosyasını import ediyoruz
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div className="min-h-screen bg-background text-textPrimary font-sans">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </div>
     </Router>
