022b33b59d9f95058382d891aff1bb44fb5c4086
DeveloperKubilay <kubiss52@yahoo.com>
2025-06-26 02:29:54 +0300

    fix: clean up import statement for index.css in Router.js

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 // import Test from './pages/test';
-import './services/index.css'; // CSS dosyasını import ediyoruz
+import './services/index.css';
 
 function App() {
   return (
