e1a71806c9692f16b6811ea12246d372fb772d92
DeveloperKubilay <kubiss52@yahoo.com>
2025-06-30 04:54:56 +0300

    Add S3 upload functionality and initial news data
    - Implemented S3 upload functionality using AWS SDK for JavaScript in backend/modules/s3.js.
    - Added a new news.json file containing a large dataset of news articles with various attributes.

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,6 @@
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const fs = require("fs");
+const s3 = require("./modules/s3");
 require("dotenv").config();
-
-const s3Client = new S3Client({
-    region: "auto",
-    endpoint: "https://a05057577967c7503e88f8229c93d296.r2.cloudflarestorage.com",
-    credentials: {
-        accessKeyId: process.env.R2_ACCESS_KEY_ID,
-        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
-    }
-});
-
-async function uploadFile(fileKey, fileData, contentType) {
-    const command = new PutObjectCommand({
-        Bucket: "haber",
-        Key: fileKey,
-        Body: fileData,
-        ContentType: contentType
-    });
-    
-    return s3Client.send(command);
-}
-
-async function main(){
-    const fileKey = "selam.png";
-    await uploadFile(fileKey, fs.readFileSync(fileKey), "image/png");
-    const url = `https://cdn.emailsunucusu.tech/${encodeURIComponent(fileKey)}`;
-    console.log("Dosya yüklendi! Url burada:", url);
-}
-
-main()
+//https://gnews.io/dashboard
+//https://newsapi.org/
+//
